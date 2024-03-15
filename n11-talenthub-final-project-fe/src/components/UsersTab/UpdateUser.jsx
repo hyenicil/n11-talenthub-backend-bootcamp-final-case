5a3adc089fb6e402df37627a5732459da2cf647e
mehmetakif.tanisik <mehmetakif.tanisik@solmaz.com>
2024-03-15 05:49:22 +0300

    add validation for update user form

--- n11-talenthub-final-project-fe/src/components/UsersTab/UpdateUser.jsx
+++ n11-talenthub-final-project-fe/src/components/UsersTab/UpdateUser.jsx
@@ -14,19 +14,23 @@ import {
   Input,
   FormLabel,
   Select,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 import { useState } from 'react';
 import { userAxios } from '../../utils/base-axios';
 
 const UpdateUser = ({ afterSave, user }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure({onClose: () =>  setErrors({})});
   const [values, setValues] = useState(user);
-
+  const [errors, setErrors] = useState({});
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await userAxios.patch(`/${user.id}`, values);
-    afterSave();
-    onClose();
+    await userAxios.patch(`/${user.id}`, values).then(() => {afterSave();
+      onClose();}).catch((data) => {
+        setErrors(data.response.data.data.details);
+      });
+    
   };
 
   const handleChange = (key) => (e) => {
@@ -49,33 +53,37 @@ const UpdateUser = ({ afterSave, user }) => {
           <ModalBody>
             <form onSubmit={handleSubmit}>
               <Stack>
-                <FormControl>
+                <FormControl isInvalid={errors.name}>
                   <FormLabel>Name</FormLabel>
                   <Input
                     defaultValue={user.name}
                     onChange={handleChange('name')}
                   />
+                  <FormErrorMessage>{errors.name}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={errors.surname}>
                   <FormLabel>Surname</FormLabel>
                   <Input
                     defaultValue={user.surname}
                     onChange={handleChange('surname')}
                   />
+                  <FormErrorMessage>{errors.surname}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={errors.email}>
                   <FormLabel>Email</FormLabel>
                   <Input
                     defaultValue={user.email}
                     onChange={handleChange('email')}
                   />
+                  <FormErrorMessage>{errors.email}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
+                <FormControl isInvalid={errors.birthDate}>
                   <FormLabel>Birth date</FormLabel>
                   <Input
                     defaultValue={user.birthDate? user.birthDate : ""}
                     onChange={handleChange('birthDate')}
                   />
+                  <FormErrorMessage>{errors.birthDate}</FormErrorMessage>
                 </FormControl>
                 <FormControl>
                   <FormLabel>Gender</FormLabel>
@@ -87,6 +95,7 @@ const UpdateUser = ({ afterSave, user }) => {
                     <option value='MALE'>Male</option>
                     <option value='FEMALE'>Female</option>
                   </Select>
+                  <FormErrorMessage>{errors.gender}</FormErrorMessage>
                 </FormControl>
                 <Button type='submit'>Update</Button>
               </Stack>

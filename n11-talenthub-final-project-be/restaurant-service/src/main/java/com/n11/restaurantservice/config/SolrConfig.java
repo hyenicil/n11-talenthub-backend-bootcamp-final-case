7e7ed24bfebf0d88a35db0d32d6d0e1858f703cf
mehmetakif.tanisik <mehmetakif.tanisik@solmaz.com>
2024-03-15 05:38:00 +0300

    separate solr config bean creation to solve test issues

---
+++ n11-talenthub-final-project-be/restaurant-service/src/main/java/com/n11/restaurantservice/config/SolrConfig.java
@@ -0,0 +1,28 @@
+package com.n11.restaurantservice.config;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.repository.config.EnableSolrRepositories;
+
+/**
+ * @author Mehmet Akif Tanisik
+ */
+@Configuration
+@EnableSolrRepositories(basePackages = "com.n11.restaurantservice")
+public class SolrConfig {
+
+    @Bean
+    public SolrClient solrClient() {
+        return new HttpSolrClient.Builder("http://localhost:8983/solr").build();
+    }
+
+    @Bean
+    public SolrTemplate solrTemplate(SolrClient client) {
+        return new SolrTemplate(client);
+    }
+
+
+}

bdbcc75e4e9e7c179f17c25d527f194265033d96
mehmetakif.tanisik <mehmetakif.tanisik@solmaz.com>
2024-03-15 05:49:51 +0300

    add style changes for main App

--- n11-talenthub-final-project-fe/src/components/AppTabs.jsx
+++ n11-talenthub-final-project-fe/src/components/AppTabs.jsx
@@ -5,11 +5,11 @@ import AddressesTab from './AddressesTab/AdressesTab';
 
 const AppTabs = () => {
   return (
-    <Tabs isLazy p={12} gap={12}>
-      <TabList>
-        <Tab>Users</Tab>
-        <Tab>Addresses</Tab>
-        <Tab>Restaurants</Tab>
+    <Tabs isFitted variant='soft-rounded' isLazy px={12} py={2} gap={12}>
+      <TabList p={2} bgColor={'primary.50'} rounded={'full'}>
+        <Tab  _selected={{ color: 'white', bg: 'primary.500' }}>Users</Tab>
+        <Tab  _selected={{ color: 'white', bg: 'primary.500' }}>Addresses</Tab>
+        <Tab  _selected={{ color: 'white', bg: 'primary.500' }}>Restaurants</Tab>
       </TabList>
 
       <TabPanels>

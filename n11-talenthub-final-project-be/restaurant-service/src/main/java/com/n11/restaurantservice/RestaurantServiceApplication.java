32962acdd12fccc2e9d415ad916870431a6acd9c
mehmetakif.tanisik <mehmetakif.tanisik@solmaz.com>
2024-03-15 05:40:14 +0300

    delete package scan from main app and send it to solrConfig

--- n11-talenthub-final-project-be/restaurant-service/src/main/java/com/n11/restaurantservice/RestaurantServiceApplication.java
+++ n11-talenthub-final-project-be/restaurant-service/src/main/java/com/n11/restaurantservice/RestaurantServiceApplication.java
@@ -3,10 +3,8 @@ package com.n11.restaurantservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 
 @SpringBootApplication
-@EnableSolrRepositories(value = "com.n11.restaurantservice")
 @EnableDiscoveryClient
 public class RestaurantServiceApplication {
 
